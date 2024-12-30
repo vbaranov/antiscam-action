4966ba29f87f67a7656cbdd083ff66425fdcd41b
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-12-30 13:51:43 +0300

    Update parsing logic

--- pkg/antiscam/body.go
+++ pkg/antiscam/body.go
@@ -5,13 +5,14 @@ import "strings"
 func checkComment(body string) []Detection {
 	var detections []Detection
 	body_lower_case := strings.ToLower(body)
-	if ((strings.Contains(body_lower_case, "https://") &&
+	if strings.Contains(body_lower_case, ".web.app") || ((strings.Contains(body_lower_case, "https://") &&
 		!strings.Contains(body_lower_case, "https://github.com") &&
 		!strings.Contains(body_lower_case, "https://discord.gg/blockscout")) ||
 		strings.Contains(body_lower_case, "http://")) &&
 		(strings.Contains(body_lower_case, "support") ||
 			strings.Contains(body_lower_case, "forum") ||
-			strings.Contains(body_lower_case, "help")) {
+			strings.Contains(body_lower_case, "help") ||
+			strings.Contains(body_lower_case, "dapps portal")) {
 		detections = append(detections, Detection{
 			Location:  "body",
 			DebugInfo: "Comment body contains scammy text.",
