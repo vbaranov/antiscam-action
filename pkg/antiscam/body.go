2577449425499a7223522a210e10e9397f4b51a9
Viktor Baranov <baranov.viktor.27@gmail.com>
2025-03-20 11:50:49 +0300

    Update parsing logic

--- pkg/antiscam/body.go
+++ pkg/antiscam/body.go
@@ -6,12 +6,14 @@ import (
 	"strings"
 )
 
+const ScammyTextDebugInfo = "Comment body contains scammy text."
+
 var whitelisted_logins = map[string]bool{}
 
 func checkComment(body string, comment_author string) []Detection {
 	fmt.Printf("comment author: %s\n", comment_author)
-	fmt.Printf("env SCAM_ACTION_WHITELISTED_LOGINS value: %s\n", os.Getenv("SCAM_ACTION_WHITELISTED_LOGINS"))
 	env_whitelisted_logins := os.Getenv("SCAM_ACTION_WHITELISTED_LOGINS")
+	fmt.Printf("env SCAM_ACTION_WHITELISTED_LOGINS value: %s\n", env_whitelisted_logins)
 	if env_whitelisted_logins != "" {
 		for _, login := range strings.Split(env_whitelisted_logins, ",") {
 			whitelisted_logins[strings.ToLower(strings.TrimSpace(login))] = true
@@ -21,14 +23,12 @@ func checkComment(body string, comment_author string) []Detection {
 	var detections []Detection
 	if !whitelisted_logins[strings.ToLower(comment_author)] {
 		body_lower_case := strings.ToLower(body)
-		scammyPatterns := []string{
+		solidScammyPatterns := []string{
 			".web.app",
-			"https://",
-			"http://",
-			".com",
 			"telegram",
 			"@",
 		}
+		scammyPatterns := append(solidScammyPatterns, "https://", "http://", ".com")
 		exceptions := []string{
 			"https://github.com",
 			"https://discord.gg/blockscout",
@@ -45,6 +45,9 @@ func checkComment(body string, comment_author string) []Detection {
 		for _, pattern := range scammyPatterns {
 			isScammy = true
 			if strings.Contains(body_lower_case, pattern) {
+				if contains(solidScammyPatterns, pattern) {
+					detections = appendDetection(detections)
+				}
 				if pattern == "https://" {
 					for _, exception := range exceptions {
 						if strings.Contains(body_lower_case, exception) {
@@ -60,10 +63,7 @@ func checkComment(body string, comment_author string) []Detection {
 		if isScammy {
 			for _, supportPattern := range supportPatterns {
 				if strings.Contains(body_lower_case, supportPattern) {
-					detections = append(detections, Detection{
-						Location:  "body",
-						DebugInfo: "Comment body contains scammy text.",
-					})
+					detections = appendDetection(detections)
 					break
 				}
 			}
@@ -72,4 +72,20 @@ func checkComment(body string, comment_author string) []Detection {
 		fmt.Printf("Author is whitelisted: %s\n", comment_author)
 	}
 	return detections
+}
+
+func appendDetection(detections []Detection) []Detection {
+	return append(detections, Detection{
+		Location:  "body",
+		DebugInfo: ScammyTextDebugInfo,
+	})
+}
+
+func contains(slice []string, item string) bool {
+	for _, element := range slice {
+		if element == item {
+			return true
+		}
+	}
+	return false
 }
